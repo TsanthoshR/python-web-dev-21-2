ceda17b8c07d5cd774331022b77cd40c634702cc
TSanthosh <santhosht5290@gmail.com>
2021-08-15 23:22:39 +0530

    added dunder str methods

--- new_project_outer/src/blog/models.py
+++ new_project_outer/src/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from organiser.models import Startup, Tag
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=63)
+    slug = models.CharField(max_length=63)
+    text = models.TextField()
+    pub_date = models.DateField()
+    tags = models.ManyToManyField(Tag)
+    startups = models.ManyToManyField(Startup)
+
+    def __str__(self) -> str:
+        date_string = self.pub_date.strftime("%Y-%m-%d")
+        return f"{self.title} on {date_string}"
--- new_project_outer/src/organiser/models.py
+++ new_project_outer/src/organiser/models.py
@@ -1,10 +1,43 @@
 from django.db import models
 
+
 class Tag(models.Model):
-    pass
+    name = models.CharField(max_length=31, unique=True)
+    slug = models.SlugField(
+        max_length=31,
+        unique=True,
+        help_text="A label for URL config.",
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
 
 class Startup(models.Model):
-    pass
+    name = models.CharField(max_length=31, db_index=True)
+    slug = models.SlugField(
+        max_length=31,
+        unique=True,
+        help_text="A label for URL config.",
+    )
+    description = models.TextField()
+    founded_date = models.DateField("date founded")
+    contact = models.EmailField()
+    website = models.URLField(max_length=255)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self) -> str:
+        return self.name
+
 
 class NewsLink(models.Model):
-    pass
+    title = models.CharField(max_length=31)
+    slug = models.SlugField(max_length=31)
+    pub_date = models.DateField("date published")
+    link = models.URLField(max_length=255)
+    startup = models.ForeignKey(
+        Startup, on_delete=models.CASCADE
+    )
+
+    def __str__(self) -> str:
+        return f"{self.startup}: {self.title}"
